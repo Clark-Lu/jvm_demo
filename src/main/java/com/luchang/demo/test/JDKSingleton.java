5b3c90bf7ab5a0bec1584e806104fb695bc64bc7
LuChang <496963664@qq.com>
2018-12-13 19:17:59 +0800

    multiple thread

--- src/main/java/com/luchang/demo/test/JDKSingleton.java
+++ src/main/java/com/luchang/demo/test/JDKSingleton.java
@@ -1,5 +1,11 @@
 package com.luchang.demo.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 /*
  *created by LuChang
  *2018/12/13 18:16
@@ -7,7 +13,18 @@ package com.luchang.demo.test;
 public class JDKSingleton {
     public static void main(String[] args) {
         System.out.println("调用之前");
-        System.out.println(JDKSingleton.getSingleton() == JDKSingleton.getSingleton());
+        List<Future<JDKSingleton>> list = new ArrayList<>(8);
+        for (int i = 0; i < 8; i++) {
+            CompletableFuture<JDKSingleton> future = CompletableFuture.supplyAsync(() -> JDKSingleton.getSingleton());
+            list.add(future);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            try {
+                System.out.println(JDKSingleton.getSingleton() == list.get(i).get());
+            } catch (Exception e) {
+                continue;
+            }
+        }
         System.out.println("调用之后");
     }
 
