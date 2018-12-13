b1c2199ad4d9ecd3cc7b1734e0abe33bd495be04
LuChang <496963664@qq.com>
2018-12-13 18:32:44 +0800

    add comment

--- src/main/java/com/luchang/demo/test/JDKSingleton.java
+++ src/main/java/com/luchang/demo/test/JDKSingleton.java
@@ -6,14 +6,19 @@ package com.luchang.demo.test;
  */
 public class JDKSingleton {
     public static void main(String[] args) {
+        System.out.println("调用之前");
         System.out.println(JDKSingleton.getSingleton() == JDKSingleton.getSingleton());
+        System.out.println("调用之后");
     }
 
     private JDKSingleton(){
-
+        System.out.println("单例初始化");
     }
 
     static class Holder{
+        static {
+            System.out.println("内部类初始化");
+        }
         private static JDKSingleton jdkSingleton = new JDKSingleton();
 
         public static JDKSingleton getJdkSingleton() {
